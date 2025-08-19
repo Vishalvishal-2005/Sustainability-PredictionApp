6dcc4abaf156aca20792d8d86348fb8a86fd342d
Vishal P <118256717+Vishalvishal-2005@users.noreply.github.com>
2025-08-19 22:16:58 +0530

    Update UserInterface.jsx

--- src/components/UserInterface.jsx
+++ src/components/UserInterface.jsx
@@ -73,7 +73,7 @@ const TrainAndPredict = () => {
                 {/* 🔹 Download Sample Dataset Button */}
                 <div className="text-center mb-4">
                     <a 
-                        href="/sample-dataset.csv"   // place your CSV in the public folder
+                        href="public/sustainability_dataset (1).csv"   // place your CSV in the public folder
                         download
                         className="btn btn-outline-info"
                     >
